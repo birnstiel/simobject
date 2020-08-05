422596d17142baea188ec8ec7fe79fe35a143295
birnstiel <birnstiel@me.com>
2020-08-05 11:56:32 +0200

    version bump

--- simobject/__init__.py
+++ simobject/__init__.py
@@ -3,7 +3,7 @@ from .simulation import Simulation
 from .updater import Updater, DataUpdater
 from .heartbeat_object import HeartbeatObject
 
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 __all__ = [
     'Quantity',
